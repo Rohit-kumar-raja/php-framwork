8acd86c8305aec75edd8f8f25645862bff85add2
raja <raja@raja>
2022-04-14 21:41:06 +0530

    all doen

--- main.php
+++ main.php
@@ -5,9 +5,5 @@ include './methods/crud.php';
 include './methods/pagination.php';
 include './methods/search_data.php';
 include './methods/table.php';
-if (isset($_POST['name']) && $_POST['name'] != '') {
-
-    insertAll('address', $_POST);
-}
 
 ?>
