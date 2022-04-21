74157da982e64f4d762edfa4fb08ee15e247c70b
raja <raja@raja>
2022-04-21 08:50:37 +0530

    all doen

--- main.php
+++ main.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
 <?php
 
 require 'config.php';
@@ -7,6 +9,14 @@ include 'methods/search_data.php';
 include 'methods/table.php';
 include 'methods/login.php';
 
-$post=['admin_username'=>'admin','admin_password'=>'Rohit83013@#'];
-echo login('tbl_admin','admin_username','admin_password',$post);
 ?>
+<table class="table table-striped">
+    <?php tableGen('tbl_university_details',['university_details_financial_start_date','university_details_financial_end_date','university_details_academic_start_date'],['university_details_financial_start_date','university_details_financial_end_date','university_details_academic_start_date'],'university_details_id','true','','','lol');
+ ?>
+</table>
+<style>
+    .id13{
+        display: none;
+    }
+</style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- methods/table.php
+++ methods/table.php
@@ -59,7 +59,7 @@ function tdGen($table_name,  $edit_url = '', $delete_url = '', $show_url = '', $
     if you want to display one 1 or 4 or 10 and any more simply pass all the table row into the table 
     th_array and into the td_array simply pass the table column name
 */
-function tableGen($table_name, $th_array, $td_array, $id='id', $action = 'false', $edit_url = '', $delete_url = '', $show_url = '', $condition = 1)
+function tableGen($table_name, $th_array, $td_array, $id = 'id', $action = 'false', $edit_url = '', $delete_url = '', $show_url = '', $condition = 1)
 {
     global $conn;
     $sno = 1;
@@ -88,7 +88,10 @@ function tableGen($table_name, $th_array, $td_array, $id='id', $action = 'false'
         }
         echo "<td>";
         if ($show_url != '') {
-            echo ' <a href="' . $show_url . '?show=' . $table_row[$id] . '"  class="btn btn-primary btn-sm  fas fa-eye "> show</a>';
+            echo '<button type="button" class="btn btn-primary btn-sm  fas fa-eye " data-bs-toggle="modal" data-bs-target="#staticBackdrop' . $table_row[$id] . '">
+              show
+        </button>';
+            data_show($table_name, $show_url, $condition);
         }
         if ($edit_url != '') {
             echo ' <a href="' . $edit_url . '?edit=' . $table_row[$id] . '"  class="btn btn-warning btn-sm fas fa-edit "> edit</a>';
@@ -102,3 +105,59 @@ function tableGen($table_name, $th_array, $td_array, $id='id', $action = 'false'
     echo "</tbody>"; //table body end
 
 }
+
+function data_show($table_name, $modal_title = 'modal', $condition = 1)
+{
+
+    global $conn;
+    global $database;
+    $th_query = "SELECT `COLUMN_NAME` FROM information_schema.columns WHERE `table_name`='$table_name' && `table_schema`='$database' ORDER BY 'id' DESC ";
+    $th_result = mysqli_query($conn, $th_query);
+   
+    $td_query = "SELECT * FROM `$table_name` WHERE $condition ";
+    $td_result = mysqli_query($conn, $td_query);
+    // this is the id of the table for goes into the next page
+    $id = mysqli_fetch_array($th_result)[0];
+    while ($td_row = mysqli_fetch_array($td_result)) {
+
+?>
+
+
+        <div class="modal fade" id="staticBackdrop<?= $td_row[$id] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel"><?= $modal_title ?></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <?php
+                             $sno = 0;
+                              foreach ($th_result as $th_row) {
+                                if ($th_row['COLUMN_NAME'] != $id) { ?>
+                                    <div class="col-sm-4 mb-2 <?= 'id'.$sno ?>">
+                                        <?php
+                                        echo '<label>' . $th_row['COLUMN_NAME'] . '</label> <br>';
+                                        echo '<input readonly type="text" class="form-control" value="' . $td_row[$th_row['COLUMN_NAME']] . '" name="" id="">';
+
+                                        ?>
+                                    </div>
+                            <?php      }
+                            $sno++;
+                            }
+                            ?>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php
+
+    }
+}
+?>
